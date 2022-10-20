017b93ff03ff71c5aa1055bb955bb09364e59f3e
niujiming <709366384@qq.com>
2022-10-20 17:18:44 +0800

    upload

--- 字符串和文本/字符串和文本.py
+++ 字符串和文本/字符串和文本.py
@@ -21,4 +21,70 @@ def example_2():
     # 通过fnmatchcase来严格区分大小写，忽略系统差异
 
 
-example_2()
+# example_2()
+
+"""
+正则非贪婪匹配
+"""
+def example_3():
+    import re
+    text = 'Computer says "no." Phone says "yes."'
+    # 默认是贪婪匹配.*会匹配到多余字符串
+    print(re.findall(r'\"(.*)\"', text))
+    # 加一个？来限制贪婪匹配
+    print(re.findall(r'\"(.*?)\"', text))
+
+# example_3()
+"""
+字符串替换
+"""
+def example_4():
+    # 简单的替换首选replace。高效
+    print("abcd".replace('a', '1'))
+    # 复杂规则可以使用translate通过建立对照表来替换
+    # 对照表需要转换成ascii值
+    table = {ord('a'): ord('1'),
+             ord('b'): ord('2'),
+             ord('c'): ord('3')}
+    print("abcd".translate(table))
+    # 使用maketrans生成对照表
+    intab = "abc"
+    outtab = "123"
+    trantab = str.maketrans(intab, outtab)
+    print("abcd".translate(trantab))
+
+# example_4()
+
+"""
+format对齐字符串
+"""
+
+def example_5():
+    # >右对齐 <左对齐 ^居中对齐
+    # 使用format代替rjust() center()等
+    # 使用=号填充20个字符
+    print(format('hello world!', '=^20'))
+
+# example_5()
+
+"""
+通过迭代器拼接字符串并限制返回最大值
+"""
+
+def example_6():
+    def combine(source, maxsize):
+        parts = []
+        size = 0
+        for part in source:
+            parts.append(part)
+            size += len(part)
+            if size > maxsize or size == maxsize:
+                yield ''.join(parts)
+                parts = []
+                size = 0
+        yield ''.join(parts)
+
+    for i in combine(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'], 5):
+        print(i)
+
+example_6()
